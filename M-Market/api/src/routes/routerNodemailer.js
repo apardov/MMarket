c23376a299640962521ddaf8fdd1b7491549a6eb
apardov <67979123+apardov@users.noreply.github.com>
2023-08-14 04:08:30 -0400

    Delete M-Market directory

--- M-Market/api/src/routes/routerNodemailer.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const { thirdPartyRegistration } = require('../handlers/routerNodemailer')
-
-const routerNodemailer = Router();
-
-routerNodemailer.post("/register", thirdPartyRegistration);
-
-module.exports = routerNodemailer
